dfbccc6448cd89d63a5917b0596d2241f633ec2d
Jordan Brown <jmbrown@andrew.cmu.edu>
2017-02-13 02:35:24 -0500

    Disallow submissions after submission phase is over

--- Handler/Upload.hs
+++ Handler/Upload.hs
@@ -6,6 +6,7 @@ import qualified Util as Util
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import Data.Conduit.Binary
+import qualified Data.ConferencePhase as C
 
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Yesod.Form.Jquery (jqueryAutocompleteField)
@@ -30,33 +31,39 @@ postUploadR = do
     reviewerOpts <- Util.reviewerOpts
     (uid, user) <- requireAuthPair
     ((result, _), _) <- runFormPost $ uploadForm (userEmailAddress user) reviewerOpts
-    case result of
-        FormSuccess (FileForm fi title a1 a2 a3 a4 abstract mConflicts) -> do
-            -- I'm sad that I had to do this, but making a custom field was hard!
-            let authors = [a1] ++ (aToList a2) ++ (aToList a3) ++ (aToList a4)
-            case not (fileContentType fi == "application/pdf") of
-                True -> do
-                    setMessage "File must be a PDF"
-                    redirect UploadR
-                False -> do
-                    fileBytes <- runResourceT $ fileSource fi $$ sinkLbs
-                    -- We get the author ids first so that we can fail if some email was incorrect before adding the paper
-                    authorIds  <- mapM (getUserForUsername UploadR) authors
-                    let authorsAndIds = zip authors authorIds
-                    paperId <- runDB $ insert $ Paper uid (fileName fi)
-                            title (unTextarea abstract)
-                                 (S.pack . L.unpack $ fileBytes) False False
-                    _ <- runDB $ mapM (\(author, Entity aUid _a) -> insert_ $ Author author aUid paperId) authorsAndIds
-                    case mConflicts of
-                        Just conflicts -> do
-                            _ <- runDB $ mapM (\conflict -> insert_ $ Conflict paperId conflict) conflicts
-                            return ()
-                        _ -> do
-                           return ()
-                    setMessage "PDF saved"
-                    redirect HomeR
+    Entity _pid phase <- getCurrentPhase
+    case currentPhasePhase phase of
+        C.Submission ->
+            case result of
+                FormSuccess (FileForm fi title a1 a2 a3 a4 abstract mConflicts) -> do
+                    -- I'm sad that I had to do this, but making a custom field was hard!
+                    let authors = [a1] ++ (aToList a2) ++ (aToList a3) ++ (aToList a4)
+                    case not (fileContentType fi == "application/pdf") of
+                        True -> do
+                            setMessage "File must be a PDF"
+                            redirect UploadR
+                        False -> do
+                            fileBytes <- runResourceT $ fileSource fi $$ sinkLbs
+                            -- We get the author ids first so that we can fail if some email was incorrect before adding the paper
+                            authorIds  <- mapM (getUserForUsername UploadR) authors
+                            let authorsAndIds = zip authors authorIds
+                            paperId <- runDB $ insert $ Paper uid (fileName fi)
+                                    title (unTextarea abstract)
+                                        (S.pack . L.unpack $ fileBytes) False False
+                            _ <- runDB $ mapM (\(author, Entity aUid _a) -> insert_ $ Author author aUid paperId) authorsAndIds
+                            case mConflicts of
+                                Just conflicts -> do
+                                    _ <- runDB $ mapM (\conflict -> insert_ $ Conflict paperId conflict) conflicts
+                                    return ()
+                                _ -> do
+                                return ()
+                            setMessage "PDF saved"
+                            redirect HomeR
+                _ -> do
+                    setMessage "Something went wrong"
+                    redirect UploadR 
         _ -> do
-            setMessage "Something went wrong"
+            setMessage "Sorry, we are no longer accepting submissions."
             redirect UploadR 
 
 getUploadR :: Handler Html
